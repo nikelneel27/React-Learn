b96e78582cd9741b341549193794df24f3841536
Nikhil <nik@Nikhil.local>
2021-05-13 18:42:24 +0530

    todo-app-commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([""]);
+  const [todo, setTodo] = useState("");
+
+
+  useEffect(() => {
+    const data = localStorage.getItem("todos");
+    const localTodos = JSON.parse(data);
+
+    if (localTodos) {
+      // setTodos(localTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    const data = JSON.stringify(todos);
+    // localStorage.setItem("todos", data);
+
+  }, [todos]);
+
+  const handleSubmit = ((e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+    }
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  });
+
+  const deleteTodo = (id) => {
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <h1>What is the plan for the day ? </h1>
+
+      <form className="todo-app-form" onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder="Enter the text here !"
+          required
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add Task</button>
+      </form>
+
+      <div className="todo-list">
+        {todos.map((todo) =>
+          <div key={todo.id}>
+            {console.log("dfgh", todo)}
+            <div className="todo-text-list">
+              {todo.text}
+            </div>
+            <button id="delete-button" onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>)}
+      </div>
     </div>
   );
 }
